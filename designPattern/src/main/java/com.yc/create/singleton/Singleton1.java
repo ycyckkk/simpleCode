6535bda0c643a9ff57826cdf18215c1183c53884
于铖 <yuchengcida@sina.com>
2020-11-29 15:05:29 +0800

    1111

---
+++ designPattern/src/main/java/com.yc/create/singleton/Singleton1.java
@@ -0,0 +1,17 @@
+package create.singleton;
+
+public class Singleton1 {
+    volatile static Singleton1 singleton1 = null;
+
+    public static Singleton1 getInstance() {
+        if (singleton1 == null) {
+            synchronized (Singleton1.class) {
+                if (singleton1 == null) {
+                    singleton1 = new Singleton1();
+                }
+            }
+        }
+        return singleton1;
+    }
+
+}
---
+++ designPattern/src/main/java/com.yc/create/singleton/SingletonTest.java
@@ -0,0 +1,7 @@
+package create.singleton;
+
+public class SingletonTest {
+    public static void main(String[] args) {
+        System.out.println(Singleton1.getInstance() == Singleton1.getInstance());
+    }
+}
